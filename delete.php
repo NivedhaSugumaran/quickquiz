e630ebd2faa26459e43609dae5aec51bc8f2262c
NivedhaSugumaran <43202131+NivedhaSugumaran@users.noreply.github.com>
2021-03-27 13:18:22 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,43 +1,23 @@
 <?php
-// Process delete operation after confirmation
 if(isset($_POST["qno"]) && !empty($_POST["qno"])){
-    // Include config file
     require_once "connection.php";
-    
-    // Prepare a delete statement
     $sql = "DELETE FROM questions WHERE qno = ?";
-    
     if($stmt = mysqli_prepare($conn, $sql)){
-        // Bind variables to the prepared statement as parameters
         mysqli_stmt_bind_param($stmt, "i", $param_id);
-        
-        // Set parameters
         $param_id = trim($_POST["qno"]);
-        
-        // Attempt to execute the prepared statement
         if(mysqli_stmt_execute($stmt)){
             echo '<script> alert(" Records deleted successfully. ");</script>';
             header("location: allquestions.php");
             exit();
         }
-    
- //set @autoid:=0 ;UPDATE questions set qno= @autoid:= (@autoid +1) ;ALTER table questions Auto_increment=1;
-	
 	else
 	{
 		echo '<script>alert("failed")</script>';
 	
 	}
-    // Close statement
     mysqli_stmt_close($stmt);
-    
-    // Close connection
     mysqli_close($conn);
-	} else{
-    // Check existence of id parameter
-    if(empty(trim($_GET["qno"]))){
-        // URL doesn't contain id parameter. Redirect to error page
-        header("location: allquestions.php");
+	} else{if(empty(trim($_GET["qno"]))){header("location: allquestions.php");
         exit();
     }
 }}
