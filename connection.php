f2f56c053645b2574f28eb2e161ca78784f2158d
NivedhaSugumaran <43202131+NivedhaSugumaran@users.noreply.github.com>
2021-03-27 12:53:39 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "php-kuiz") or die("could not connect" . mysqli_error($conn) ) ;
+$conn = mysqli_connect("ls-368e16b6922a409f3c8f6c38fa4222ae912f9d6d.ct5zdyhxsqlg.ap-south-1.rds.amazonaws.com", "admin", "12345678", "quiz") or die("could not connect" . mysqli_error($conn) ) ;
 if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
